b5f922c669397f18c8739d849665715514e381fc
vhuangyucheng <1063215202@qq.com>
2017-10-23 22:33:09 +0800

    1，分别在filter和exception准备增加自定义过滤器和异常类 2，添加了REST的支持包

---
+++ kooing-framework-filter/src/main/java/com/kooing/filter/BaseAuthFilter.java
@@ -0,0 +1,18 @@
+package com.kooing.filter;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import java.io.IOException;
+
+/**
+ * @author : kooing
+ * @Date : 2017/10/23 - 22:22
+ * @Desription :
+ * @update by :
+ */
+public class BaseAuthFilter implements ContainerRequestFilter {
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+
+    }
+}
--- kooing-framework-param/src/main/java/com/kooing/framework/param/common/request/BaseReq.java
+++ kooing-framework-param/src/main/java/com/kooing/framework/param/common/request/BaseReq.java
@@ -14,18 +14,26 @@ import java.io.Serializable;
 public class BaseReq implements Serializable{
 
     private static final long serialVersionUID = 6947098193840850628L;
-
-    protected String operType;//操作类型add表示增加，edit表示修改
-    protected String operUserId;//操作用户id
-    protected String operUserName;//操作用户的昵称
+    /** 操作用户id */
+    protected String operUserId;
+    /** 操作类型add表示增加，edit表示修改 */
+    protected String operType;
+    /** 操作人*/
+    protected String operUserName;
     protected String page;
     protected String rows;
 
     protected String startTime;
     protected String endTime;
 
-    //暂时不知道什么用的
-    protected void WritePageClient(Request request, Object commResp) {
+
+    /**
+     * @Author      : kooing
+     * @Date        : 2017/10/17 21:29
+     * @Desription  : 暂时不知道什么用的
+     * @return      :
+     */
+    protected void writePageClient(Request request, Object commResp) {
         JSONObject json = JSONObject.fromObject(commResp);
         Response.status(Response.Status.OK).entity(json).type("text/plain;charset=UTF-8").build();
 
